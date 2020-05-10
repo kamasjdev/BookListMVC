dee1949b4e72a29ce36713e001720723648197ff
KW <KW*KW>
2020-05-10 20:28:50 +0200

    Adding MVC Project

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> BooksMVC{ get; set; } // model Book bedzie nazwany jako BooksMVC w bazie danych
+    }
+}
